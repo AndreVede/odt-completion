15597d08ea441da48ef83cda0e8dbb0f23f31794
Zacharie Boisnard <mecazack@gmail.com>
2022-10-07 08:11:35 +0200

    Set template

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var odtCompletion = module.exports = {
-    
+var fs = require('fs');
+
+class Odt {
+    constructor(template){
+        /* An odt is a zip, and when you extract an odt you have a file : content.xml which is our template */
+        this.template = template;
+    };
 }
+
+/* An odt is a zip, and when you extract an odt you have a file : content.xml which is our template */
+exports.setTemplate = function(template) {
+    Odt.template = template;
+};
