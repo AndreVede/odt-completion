972f379fe97abf11817196555da9ebf1e9196c96
Zacharie Boisnard <mecazack@gmail.com>
2022-10-07 08:13:52 +0200

    correction

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ class Odt {
         /* An odt is a zip, and when you extract an odt you have a file : content.xml which is our template */
         this.template = template;
     };
-}
-
-/* An odt is a zip, and when you extract an odt you have a file : content.xml which is our template */
-exports.setTemplate = function(template) {
-    Odt.template = template;
+    setTemplate = function(template) {
+        this.template = template;
+    };
 };
