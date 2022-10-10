838b86aa97d26f6ddb90e8568108bd3df4c9cbb5
Zacharie Boisnard <mecazack@gmail.com>
2022-10-10 23:38:28 +0200

    ajout du template

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var fsPromises = require('fs').promises;
+
 var jszip = require('jszip');
 
 var odtOut = new jszip();
@@ -6,16 +8,35 @@ class Odt {
     constructor(template){
         /* An odt is a zip, and when you extract an odt you have a file : content.xml which is our template */
         try {
-            this.template = fs.readFileSync(template);
+            this.template = fs.readFileSync('content.xml', template);
+            this.writeTemplate = fs.writeFileSync('content.xml', template);
         } catch (err) {
             console.error(err);
         };
     };
     setTemplate = function(template) {
         try {
-            this.template = fs.readFileSync(template);
+            this.template = fs.readFileSync('content.xml', template);
+            this.writeTemplate = fs.writeFileSync('content.xml', template);
         } catch (err) {
             console.error(err);
         };
+    };
+};
+
+exports.getOdtObject = function (odtFile) {
+    var file = new ArrayBuffer();
+    try {
+        //passage de odt à zip
+        var odtZip = odtFile.substring(0, odtFile.length - 4);
+        odtZip =+ '.zip'        
+        fsPromises.rename(odtFile, odtZip);
+
+        //lecture de content.xml
+        zip.file('content.xml').async("string").then(function (data) {
+            return new Odt(data);
+        });
+    } catch (err) {
+        console.error(err);
     };
 };
