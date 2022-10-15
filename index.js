d198b35241a66147248beabfa7bf5a4815f475f4
Zacharie Boisnard <mecazack@gmail.com>
2022-10-15 17:04:10 +0200

    feat: préparation à l'ajout du remplacement de variable

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var jszip = require('jszip');
 
 var odtOut = new jszip();
 class Odt {
-    constructor(template){
+    constructor(template, odtZip){
         /* An odt is a zip, and when you extract an odt you have a file : content.xml which is our template */
         try {
             this.template = fs.readFileSync('content.xml', template);
             this.writeTemplate = fs.writeFileSync('content.xml', template);
+            this.odtZip = odtZip; //odtZip est l'odt en format zip
         } catch (err) {
             console.error(err);
         };
@@ -36,10 +37,19 @@ exports.getOdtObject = function (odtFile) {
         var zip = new jszip();
         zip.loadAsync(odtZip).then(function(zip) {
             zip.file('content.xml').async("string").then(function (data) {
-                return new Odt(data);
+                return new Odt(data, odtZip);
             });
         });
     } catch (err) {
         console.error(err);
+    };
+};
+
+exports.changeVariable = function (odtObject, variableName, variable) {
+    if (odtObject.template !== null && odtObject.template !== undefined && odtObject.writeTemplate !== null && odtObject.writeTemplate !== undefined) {
+        // recherche de l'emplacement de la variable dans le content.xml, variable stockée comme ça : {{variable}}
+
+    } else {
+        console.error("Il n'y a pas de template dans votre objet");
     };
 };
