dc622485a62a43bc8162d0b32bc0b5e335cca183
Zacharie Boisnard <mecazack@gmail.com>
2022-10-16 01:54:20 +0200

    feat: création des méthodes importantes

--- index.js
+++ index.js
@@ -1,39 +1,81 @@
+"use strict";
+
 var fs = require('fs');
 var fsPromises = require('fs').promises;
 
 var jszip = require('jszip');
-
-var odtOut = new jszip();
 class Odt {
+    template;
+    odtFile;
+    odtZip;
+
     constructor(odtFile){
         /* An odt is a zip, and when you extract an odt you have a file : content.xml which is our template */
-        var file = new ArrayBuffer();
         try {
             //passage de odt à zip
             var odtZip = odtFile.substring(0, odtFile.length - 4);
-            odtZip =+ '.zip'        
+            odtZip = odtZip + '.zip';
+                    
             fsPromises.rename(odtFile, odtZip);
+
+            this.odtZip = odtZip; //odtZip est l'odt en format zip
+            this.odtFile = odtFile;
 
             //lecture de content.xml
             var zip = new jszip();
-            zip.loadAsync(odtZip).then(function(zip) {
-                zip.file('content.xml').async("string").then(function (data) {
-                    this.template = fs.readFileSync('content.xml', data);
-                    this.writeTemplate = fs.writeFileSync('content.xml', data);
-                    this.odtZip = odtZip; //odtZip est l'odt en format zip
+            var dataTemplate = '';
+            fs.readFile(odtZip, function (err, zipData) {
+                if (err) throw err;
+                zip.loadAsync(zipData).then(function(zip) {
+                    zip.file('content.xml').async("string").then(function (data) {
+                        this.template = data;
+                    });
                 });
             });
+
+            this.template = dataTemplate;
+            console.log(this.template);
+
         } catch (err) {
             console.error(err);
         };
     };
     changeVariable = function (variableName, variable) {
-        if (this.template !== null && this.template !== undefined && this.writeTemplate !== null && this.writeTemplate !== undefined) {
+        if (this.template !== null && this.template !== undefined) {
             // recherche de l'emplacement de la variable dans le content.xml, variable stockée comme ça : {{variable}}
-    
+            // regex the name
+            var variableNameRegex = new RegExp('{{' + variableName + '}}', 'g');
+            this.template.replace(variableNameRegex, variable);
         } else {
             console.error("Il n'y a pas de template dans votre objet");
         };
+    };
+    recreateOdtFile = function () {
+        if (this.template !== null && this.template !== undefined) {
+            var file = new ArrayBuffer();
+            try {
+                var zip = new jszip();
+                var outZip = new jszip();
+                fs.readFile(this.odtZip, function (err, zipData) {
+                    if (err) throw err;
+                    zip.loadAsync(zipData).then(function(zip) {
+                        outZip = zip;
+                        outZip.remove('content.xml');
+                        outZip.file('content.xml', this.template);
+                        outZip.generateNodeStream({type:'nodebuffer',streamFiles:true})
+                        .pipe(fs.createWriteStream(this.odtZip))
+                        .on('finish', function () {
+                            // JSZip generates a readable stream with a "end" event,
+                            // but is piped here in a writable stream which emits a "finish" event.
+                            console.log(this.odtZip+" written");
+                        });
+                        fsPromises.rename(this.odtZip, this.odtFile);
+                    });
+                });         
+            } catch (err) {console.error(err);};
+        } else {
+            console.error('Il faut d\'abord créer un template');
+        }
     };
 };
 module.exports = {Odt: Odt};
