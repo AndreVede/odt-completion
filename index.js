fb3b6bde9938dabf2a6e66df602fda1abceb63d5
Zacharie Boisnard <mecazack@gmail.com>
2022-10-15 22:05:41 +0200

    BREAKING CHANGES: refonte de l'architecture de la librairie

--- index.js
+++ index.js
@@ -5,51 +5,35 @@ var jszip = require('jszip');
 
 var odtOut = new jszip();
 class Odt {
-    constructor(template, odtZip){
+    constructor(odtFile){
         /* An odt is a zip, and when you extract an odt you have a file : content.xml which is our template */
+        var file = new ArrayBuffer();
         try {
-            this.template = fs.readFileSync('content.xml', template);
-            this.writeTemplate = fs.writeFileSync('content.xml', template);
-            this.odtZip = odtZip; //odtZip est l'odt en format zip
-        } catch (err) {
-            console.error(err);
-        };
-    };
-    setTemplate = function(template) {
-        try {
-            this.template = fs.readFileSync('content.xml', template);
-            this.writeTemplate = fs.writeFileSync('content.xml', template);
-        } catch (err) {
-            console.error(err);
-        };
-    };
-};
+            //passage de odt à zip
+            var odtZip = odtFile.substring(0, odtFile.length - 4);
+            odtZip =+ '.zip'        
+            fsPromises.rename(odtFile, odtZip);
 
-exports.getOdtObject = function (odtFile) {
-    var file = new ArrayBuffer();
-    try {
-        //passage de odt à zip
-        var odtZip = odtFile.substring(0, odtFile.length - 4);
-        odtZip =+ '.zip'        
-        fsPromises.rename(odtFile, odtZip);
-
-        //lecture de content.xml
-        var zip = new jszip();
-        zip.loadAsync(odtZip).then(function(zip) {
-            zip.file('content.xml').async("string").then(function (data) {
-                return new Odt(data, odtZip);
+            //lecture de content.xml
+            var zip = new jszip();
+            zip.loadAsync(odtZip).then(function(zip) {
+                zip.file('content.xml').async("string").then(function (data) {
+                    this.template = fs.readFileSync('content.xml', data);
+                    this.writeTemplate = fs.writeFileSync('content.xml', data);
+                    this.odtZip = odtZip; //odtZip est l'odt en format zip
+                });
             });
-        });
-    } catch (err) {
-        console.error(err);
+        } catch (err) {
+            console.error(err);
+        };
+    };
+    changeVariable = function (variableName, variable) {
+        if (this.template !== null && this.template !== undefined && this.writeTemplate !== null && this.writeTemplate !== undefined) {
+            // recherche de l'emplacement de la variable dans le content.xml, variable stockée comme ça : {{variable}}
+    
+        } else {
+            console.error("Il n'y a pas de template dans votre objet");
+        };
     };
 };
-
-exports.changeVariable = function (odtObject, variableName, variable) {
-    if (odtObject.template !== null && odtObject.template !== undefined && odtObject.writeTemplate !== null && odtObject.writeTemplate !== undefined) {
-        // recherche de l'emplacement de la variable dans le content.xml, variable stockée comme ça : {{variable}}
-
-    } else {
-        console.error("Il n'y a pas de template dans votre objet");
-    };
-};
+module.exports = {Odt: Odt};
